2a103fecfb416d670d40a36dc3b2111f7e1b2e90
Maxim Ignatiev <thevietto@gmail.com>
2015-11-12 17:03:03 +0300

    page contents db storage

--- app/controllers/page_contents_controller.rb
+++ app/controllers/page_contents_controller.rb
@@ -1,11 +1,18 @@
 class PageContentsController < ApplicationController
 
   def edit
-
+    @content = PageContent.find(params[:id])
   end
 
   def update
+    @content = PageContent.find(params[:id])
+    @content.update(page_contents_params)
+    redirect_to root_path
+  end
 
+  private
+  def page_contents_params
+    params.require(:page_content).permit(:content)
   end
 
 end
