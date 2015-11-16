875bec3083f1efb4c77476b4e0845c3770c3a32f
ArthurShaw <thearturshaw@gmail.com>
2015-11-16 16:29:31 +0300

    Merge branch 'conf-38' of /home/arthur/RubymineProjects/conference with conflicts.

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,3 +1,6 @@
 class Workshop < ActiveRecord::Base
   belong_to :user
+  validates :title, :description, presence: true
+  has_attached_file :document
+  validates_attachment :document, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 end
