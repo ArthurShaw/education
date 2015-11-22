eea7463f29b0efc94e6f9c6f0cf679335a1aec74
Maxim Ignatiev <thevietto@gmail.com>
2015-11-22 22:05:41 +0300

    translation helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
     request_controller.start_with? 'admin'
   end
 
+  def translate_field(object, field)
+    if I18n.locale == :en && object.respond_to?((getter = field.to_s + '_en'))
+      object.method(getter).call
+    else
+      object.method(field).call
+    end
+  end
+
 end
