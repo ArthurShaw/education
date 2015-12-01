bebd037b6cd5fa77230bd984b8cf0f8fc9c0f2f6
Maxim Ignatiev <thevietto@gmail.com>
2015-12-01 18:47:58 +0300

    css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require gmaps/google
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require ckeditor/init
 //= require bootstrap
-//= require_tree .
+//= require gmaps/google
 //= require underscore
 //= require index
+//= require_tree .
