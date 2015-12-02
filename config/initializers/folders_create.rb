a60ae527992f9f970a6c9c3826c3aa0ddf9b2163
ArthurShaw <thearturshaw@gmail.com>
2015-12-02 16:21:29 +0300

    [epic-64] create folder fix

--- config/initializers/folders_create.rb
+++ config/initializers/folders_create.rb
@@ -5,13 +5,13 @@ system_folder = File.path(Rails.root.join('public', 'system'))
 uploads_folder = File.path(Rails.root.join('public', 'uploads', 'exports'))
 
 unless File.directory?(ckeditor_folder)
-  FileUtils.mkdir(ckeditor_folder)
+  FileUtils.mkdir_p(ckeditor_folder)
 end
 
 unless File.directory?(system_folder)
-  FileUtils.mkdir(system_folder)
+  FileUtils.mkdir_p(system_folder)
 end
 
 unless File.directory?(uploads_folder)
-  FileUtils.mkdir(uploads_folder)
+  FileUtils.mkdir_p(uploads_folder)
 end
