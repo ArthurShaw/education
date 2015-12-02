a4a9b5d61a8aa3c5817ea1a4aed7540e8452f0a0
ArthurShaw <thearturshaw@gmail.com>
2015-12-02 13:38:59 +0300

    [epic-64] drop columns

--- db/migrate/20151126122020_add_descr_to_sections.rb
+++ db/migrate/20151126122020_add_descr_to_sections.rb
@@ -2,5 +2,6 @@ class AddDescrToSections < ActiveRecord::Migration
   def change
     add_column :sections, :description, :text, null: false, default: ''
     add_column :sections, :description_en, :text, null: false, default: ''
+
   end
 end
