023ef5115c3c017aa073cd59648a4bed654379f5
Maxim Ignatiev <thevietto@gmail.com>
2015-12-03 14:43:14 +0300

    email settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,8 +46,8 @@ Rails.application.configure do
       :address => 'smtp.gmail.com',
       :port => 587,
       :domain => 'gmail.com',
-      :user_name => ENV["GMAIL_USERNAME"],
-      :password => ENV["GMAIL_PASS"],
+      :user_name => ENV['gmail_login'],
+      :password => ENV['gmail_pass'],
       :authentication => :login,
       :enable_starttls_auto => true
   }
