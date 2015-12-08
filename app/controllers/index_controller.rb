96785f48b8e2fb5f22cc9b6f7af0a2a5c584981e
Maxim Ignatiev <thevietto@gmail.com>
2015-12-08 14:08:08 +0300

    new picture. prevent multiple selects from db

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,13 +2,9 @@ class IndexController < ApplicationController
 
 
   def index
-    @about_conference_content = PageContent.first
-    @why_kfu_content = PageContent.second
-    @org_comitee_content = PageContent.third
-    @contacts_content = PageContent.find(5)
-    @sponsor_categories = SponsorCategory.all
-    @location_json = PageContent.find(4).content
+    @page_contents = PageContent.all
 
+    @sponsor_categories = SponsorCategory.all
     @special_quests = SpecialGuest.where(:guest_type => SpecialGuest.guest_types[:guest])
     @key_speakers = SpecialGuest.where(:guest_type => SpecialGuest.guest_types[:speaker])
     @sections = Section.order(:is_main => :desc, :title => :asc)
