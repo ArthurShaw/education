2023335e47d037e3225a123b8d541705bda409ba
ArthurShaw <thearturshaw@gmail.com>
2015-12-14 19:13:49 +0300

    [epic-64]seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,20 +12,20 @@ MailContent.create(name: 'Доклад одобрен (Докладчику)', i
 MailContent.create(name: 'Новый доклад (Администратору)', id: 4)
 MailContent.create(name: 'Докладчик зарегистрировался (Администратору)', id: 3)
 MailContent.create(name: 'Докладчик зарегистрировался (Докладчику)', id: 2)
-# MailContent.create(name: 'Слушатель зарегистрировался (Слушателю)', id: 1)
-# PageContent.create(name: 'Регламент', content: '<h1> Регламент конференции</h1>', id: 9)
-# PageContent.create(name: 'Культурная программа', content: '<h1> Cultural Program</h1>', id: 7)
-# PageContent.create(name: 'Варианты размещения', content: '<h1> Accommodation </h1>', id: 8)
-# PageContent.create(name: 'Информационное письмо', content: '<h1>ABOUT CONFERENCE</h1>', id: 6)
-# PageContent.create(id: 4, name:'Address', content: '', content_type: 1)
-# Section.create(:title => 'Общая программа', :title_en => 'Main program', :is_main => true)
-#
-# u = User.create(email: 'admin@admin.ru', password: 'password', first_name: 'Admin', last_name: 'Admin')
-# u.add_role("admin")
-# u.add_role("publisher")
-#
-# PageContent.create(name: 'Контакты', content: '<h1>Contacts page</h1>', id: 5)
-# PageContent.create(name: 'Орг. коммитет', content: '<h1>Contacts page</h1>', id: 3)
-# PageContent.create(name: 'Main page', content: '<h1>Main page</h1>', id: 1)
-# PageContent.create(name: 'Contacts', content: '<h1>Contacts page</h1>', id: 2)
-#
+MailContent.create(name: 'Слушатель зарегистрировался (Слушателю)', id: 1)
+PageContent.create(name: 'Регламент', content: '<h1> Регламент конференции</h1>', id: 9)
+PageContent.create(name: 'Культурная программа', content: '<h1> Cultural Program</h1>', id: 7)
+PageContent.create(name: 'Варианты размещения', content: '<h1> Accommodation </h1>', id: 8)
+PageContent.create(name: 'Информационное письмо', content: '<h1>ABOUT CONFERENCE</h1>', id: 6)
+PageContent.create(id: 4, name:'Address', content: '', content_type: 1)
+Section.create(:title => 'Общая программа', :title_en => 'Main program', :is_main => true)
+
+u = User.create(email: 'admin@admin.ru', password: 'password', first_name: 'Admin', last_name: 'Admin')
+u.add_role("admin")
+u.add_role("publisher")
+
+PageContent.create(name: 'Контакты', content: '<h1>Contacts page</h1>', id: 5)
+PageContent.create(name: 'Орг. коммитет', content: '<h1>Contacts page</h1>', id: 3)
+PageContent.create(name: 'Main page', content: '<h1>Main page</h1>', id: 1)
+PageContent.create(name: 'Contacts', content: '<h1>Contacts page</h1>', id: 2)
+
