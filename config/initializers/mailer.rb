0e7d208c73e3887d407f1cba7ab7d4b385fdeceb
Maxim Ignatiev <thevietto@gmail.com>
2015-12-15 17:49:24 +0300

    rake fail fix

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,4 +1,6 @@
-if MailerSettings
+if ActiveRecord::Base.connection.tables.include?('settings') and !defined?(::Rake)
   mailer_settings = MailerSettings.smtp_settings
-  ActionMailer::Base.smtp_settings.merge!(mailer_settings)
+  if mailer_settings
+    ActionMailer::Base.smtp_settings.merge!(mailer_settings)
+  end
 end
