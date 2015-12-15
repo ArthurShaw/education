ef002d9658e6b942166a747351489f1a630832fe
Maxim Ignatiev <thevietto@gmail.com>
2015-12-15 10:59:51 +0300

    schedule markup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,10 +77,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # config.action_controller.asset_host = 'foss-conf.bitnamiapp.com'
   # ACTION MAILER
 
   config.action_mailer.default_url_options = {:host => 'foss-conf.bitnamiapp.com'}
-  config.action_controller.asset_host = 'foss-conf.bitnamiapp.com'
   Rails.application.routes.default_url_options[:host] = 'foss-conf.bitnamiapp.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
