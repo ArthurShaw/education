174787c013a344889048100cc1208f3ed0eb09e8
Maxim Ignatiev <thevietto@gmail.com>
2015-12-15 17:28:50 +0300

    mailer  config from admin panel

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,4 +1,4 @@
-mailer_settings = MailerSettings.smtp_settings
-if mailer_settings
+if MailerSettings
+  mailer_settings = MailerSettings.smtp_settings
   ActionMailer::Base.smtp_settings.merge!(mailer_settings)
 end
