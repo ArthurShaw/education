ecdf6d1f9c83414e77788de4e611a92fe75866f0
Maxim Ignatiev <thevietto@gmail.com>
2015-12-16 16:54:34 +0300

    assets fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_controller.asset_host = 'http://foss-conf.bitnamiapp.com:80'
+  config.action_controller.asset_host = 'http://localhost:3000'
   # ACTION MAILER
 
   config.action_mailer.default_url_options = {:host => 'foss-conf.bitnamiapp.com'}
